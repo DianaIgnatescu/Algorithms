b4c324881e254d8a4196761e1a1be82af2c7756f
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-05 18:22:00 +0100

    Implement recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,30 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batches = []
+
+    # Create lists to hold values for needed and available ingredients
+    needed_ingredients = list(recipe.values())
+    available_ingredients = list(ingredients.values())
+
+    # If there are more values in the recipe than in the ingredients return 0
+    if len(needed_ingredients) != len(available_ingredients):
+        return 0
+
+    for i in range(0, len(needed_ingredients)):
+        # Divide value in available ingredients
+        batches.append(math.floor(available_ingredients[i] // needed_ingredients[i]))
+
+        if min(batches) < 1:
+            return 0
+    return min(batches)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+    ingredients = { 'milk': 432, 'butter': 158, 'flour': 51 }
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
