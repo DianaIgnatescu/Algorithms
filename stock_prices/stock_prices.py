2fe9e5b426e563f8db45968ffd09c0e25588f768
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-05 14:38:13 +0100

    Remove several prints and comments

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,18 +2,11 @@
 
 import argparse
 
-# A bot that buys and sells amazon stock
-
-# function should return the max profit that can be made from a single buy and sell
-# find the max difference between the smallest and largest prices in the list of prices
-
-
 
 def find_max_profit(prices):
 
     # 1. Keep track of lowest price in the list
     lowest = prices[0]
-    print(lowest)
 
     # The max profit must be computed by subtracting some price by another price that comes before it
     max_profit = prices[1] - lowest
@@ -21,18 +14,16 @@ def find_max_profit(prices):
     # 2. Keep track of current min price so far and max profit so far
 
     for current_price in prices[1:]:
-        # find the highest profit in the list
+        # Find the highest profit in the list
         max_profit = max(current_price - lowest, max_profit)
-        print(current_price)
-        print(max_profit)
-        # find the smallest price in the list
+
+        # Find the smallest price in the list
         lowest = min(current_price, lowest)
-        print(lowest)
 
     return max_profit
 
 
-print(find_max_profit([300, 1500, 280, 5120, 14]))
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
 
 
 if __name__ == '__main__':
